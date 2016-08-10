04c38d0f6f7e5d556f53591bbeb494d877d304e3
Jost Neigenfind <jostie@gmx.de>
2016-08-10 18:28:08 +0200

    Minor changes.

--- src/system/ExportToERNEST.java
+++ src/system/ExportToERNEST.java
@@ -71,7 +71,7 @@ public class ExportToERNEST {
 				while (iteratorSubstrateSpecies.hasNext()){					
 					Species substrateSpecies = iteratorSubstrateSpecies.next();					
 					substrateSpeciesString = substrateSpeciesString + "'" + substrateSpecies.toString() + "'";
-					substrateStoichiometryString = substrateStoichiometryString + complexSubstrate.getNumberOfOccurences(substrateSpecies);
+					substrateStoichiometryString = substrateStoichiometryString + complexSubstrate.getNode().getNumberOfOccurences(substrateSpecies);
 					if (iteratorSubstrateSpecies.hasNext()){
 						substrateSpeciesString = substrateSpeciesString + ",";
 						substrateStoichiometryString = substrateStoichiometryString + " ";
@@ -88,7 +88,7 @@ public class ExportToERNEST {
 				while (iteratorProductSpecies.hasNext()){					
 					Species productSpecies = iteratorProductSpecies.next();					
 					productSpeciesString = productSpeciesString + "'" + productSpecies.toString() + "'";
-					productStoichiometryString = productStoichiometryString + complexProduct.getNumberOfOccurences(productSpecies);
+					productStoichiometryString = productStoichiometryString + complexProduct.getNode().getNumberOfOccurences(productSpecies);
 					if (iteratorProductSpecies.hasNext()){
 						productSpeciesString = productSpeciesString + ",";
 						productStoichiometryString = productStoichiometryString + " ";
